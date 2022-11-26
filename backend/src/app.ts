dc3e6c2a3f7749e57fe8401edefcd6dfe8bc7014
Andrei Zhulid <>
2022-11-26 16:41:51 +0100

    Installed and configured 'cors' to fix issue: "Request has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header"

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,15 @@
 import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 import SearchService from '../services/SearchService';
 
 const app: Application = express();
+const options: cors.CorsOptions = { origin: ['http://localhost:8080'] };
+
+app.use(cors(options));
 
 app.get('/search', (request: Request, response: Response) => {
   try {
