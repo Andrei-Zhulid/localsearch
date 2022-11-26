706150b7cf95cc2c2e56abc61b6560aeb0530b56
Andrei Zhulid <>
2022-11-26 17:51:20 +0100

    Extracted results row to separate component

---
+++ frontend/src/components/SearchResultsRow.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-row>
+    <SearchResultsColumn
+      v-for="item in items"
+      :key="item.id"
+    >
+      <PlaceCard :place="item"/>
+    </SearchResultsColumn>
+  </v-row>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import SearchResultsColumn from '@/components/SearchResultsColumn.vue';
+import PlaceCard from '@/components/PlaceCard.vue';
+import IPlace from '../../../common/interfaces/IPlace';
+
+@Component({ components: { PlaceCard, SearchResultsColumn } })
+export default class SearchResultsRow extends Vue {
+  @Prop({ type: Array, required: true }) items!: IPlace[];
+}
+</script>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -6,14 +6,10 @@
     <SearchInput class="ma-3"/>
 
     <v-container>
-      <v-row v-show="!loading && places?.length > 0">
-        <SearchResultsColumn
-          v-for="place in places"
-          :key="place.id"
-        >
-          <PlaceCard :place="place"/>
-        </SearchResultsColumn>
-      </v-row>
+      <SearchResultsRow
+        v-show="!loading && places?.length > 0"
+        :items="places"
+      />
       <SearchResultsRowNoData v-show="!loading && places?.length === 0"/>
       <SearchResultsRowLoader v-show="loading"/>
     </v-container>
@@ -26,11 +22,10 @@ import SearchInput from '@/components/SearchInput.vue';
 import SearchResultsRowLoader from '@/components/SearchResultsRowLoader.vue';
 import IPlace from '../../../common/interfaces/IPlace';
 import SearchService from '@/servises/SearchService';
-import SearchResultsColumn from '@/components/SearchResultsColumn.vue';
-import PlaceCard from '@/components/PlaceCard.vue';
 import SearchResultsRowNoData from '@/components/SearchResultsRowNoData.vue';
+import SearchResultsRow from '@/components/SearchResultsRow.vue';
 
-@Component({ components: { SearchResultsRowNoData, PlaceCard, SearchResultsColumn, SearchResultsRowLoader, SearchInput } })
+@Component({ components: { SearchResultsRow, SearchResultsRowNoData, SearchResultsRowLoader, SearchInput } })
 export default class HomeView extends Vue {
   private places: IPlace[] = [];
   private loading = false;
