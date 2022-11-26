75fc73b1a7bb74d2d8b49d0ca30d71ed87fd69da
Andrei Zhulid <>
2022-11-26 17:06:20 +0100

    Added loading and display all places when Home page is opened

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -6,6 +6,19 @@
     <SearchInput class="ma-3"/>
 
     <v-container>
+      <v-row v-show="!loading && places?.length > 0">
+        <SearchResultsColumn
+          v-for="place in places"
+          :key="place.id"
+        >
+          <PlaceCard :place="place"/>
+        </SearchResultsColumn>
+      </v-row>
+      <v-row v-show="!loading && places?.length === 0">
+        <SearchResultsColumn class="mx-4">
+          Your search did not match any places.
+        </SearchResultsColumn>
+      </v-row>
       <SearchResultsLoaderRow v-show="loading"/>
     </v-container>
   </div>
@@ -15,9 +28,29 @@
 import { Component, Vue } from 'vue-property-decorator';
 import SearchInput from '@/components/SearchInput.vue';
 import SearchResultsLoaderRow from '@/components/SearchResultsLoaderRow.vue';
+import IPlace from '../../../common/interfaces/IPlace';
+import SearchService from '@/servises/SearchService';
+import SearchResultsColumn from '@/components/SearchResultsColumn.vue';
+import PlaceCard from '@/components/PlaceCard.vue';
 
-@Component({ components: { SearchResultsLoaderRow, SearchInput } })
+@Component({ components: { PlaceCard, SearchResultsColumn, SearchResultsLoaderRow, SearchInput } })
 export default class HomeView extends Vue {
+  private places: IPlace[] = [];
   private loading = false;
+
+  private async search(query?: string) {
+    try {
+      this.loading = true;
+      this.places = await SearchService.search(query);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      this.loading = false;
+    }
+  }
+
+  async created() {
+    await this.search();
+  }
 }
 </script>
