74f8f1cd98ed6ea5b48fda986bde1660cf0c2694
Andrei Zhulid <>
2022-11-27 12:56:11 +0100

    Moved string route names to enum

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -4,21 +4,27 @@ import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
+enum RouteName {
+  Home = 'Home',
+  Place = 'Place',
+  NotFound = 'NotFound',
+}
+
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'home',
+    name: RouteName.Home,
     component: HomeView
   },
   {
     path: '/place/:id',
-    name: 'Place',
+    name: RouteName.Place,
     component: () => import(/* webpackChunkName: "place" */ '@/views/PlaceView.vue'),
     props: true,
   },
   {
     path: '*',
-    name: 'NotFound',
+    name: RouteName.NotFound,
     component: () => import(/* webpackChunkName: "not-found" */ '@/views/NotFound.vue'),
   },
 ]
@@ -29,4 +35,4 @@ const router = new VueRouter({
   routes
 })
 
-export default router
+export { router, RouteName }
