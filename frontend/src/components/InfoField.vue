b0e3599a1acf2cbcce51a99d78ee730273e9d412
Andrei Zhulid <>
2022-11-27 14:18:46 +0100

    Implemented markup for 'Address', 'Website', 'Phones' details on place page

---
+++ frontend/src/components/InfoField.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="font-weight-medium">{{ title }}</div>
+    <div class="ml-6 mb-4">
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class InfoField extends Vue {
+  @Prop({ type: String, required: true }) title!: string;
+}
+</script>
--- frontend/src/views/PlaceView.vue
+++ frontend/src/views/PlaceView.vue
@@ -1,6 +1,41 @@
 <template>
   <div>
-    <span v-if="place">{{ place.name }}</span>
+    <div v-if="place">
+      <div class="text-h4 mb-4">{{ place.name }}</div>
+
+      <v-container class="pa-0">
+        <v-row>
+          <v-col>
+            <InfoField
+              v-if="place.address"
+              title="Address"
+            >{{ place.address }}
+            </InfoField>
+
+            <InfoField title="Website">
+              <a
+                :href="place.website"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {{ place.website }}
+              </a>
+            </InfoField>
+
+            <InfoField :title="phoneTitle">
+              <a
+                v-for="phone in place.phones"
+                :key="phone"
+                :href="`tel:${phone}`"
+                class="d-block"
+              >
+                {{ phone }}
+              </a>
+            </InfoField>
+          </v-col>
+        </v-row>
+      </v-container>
+    </div>
     <span v-else>Loading...</span>
   </div>
 </template>
@@ -11,12 +46,17 @@ import IPlaceDetailed from '../../../common/interfaces/IPlaceDetailed';
 import SearchService from '@/servises/SearchService';
 import { AxiosError } from 'axios';
 import { RouteName } from '@/router';
+import InfoField from '@/components/InfoField.vue';
 
-@Component
+@Component({ components: { InfoField } })
 export default class PlaceView extends Vue {
   @Prop({ type: String, required: true }) readonly id!: string;
 
   private place: IPlaceDetailed | null = null;
+
+  private get phoneTitle() {
+    return (this.place?.phones?.length ?? 0) > 1 ? 'Phones' : 'Phone'
+  }
 
   // noinspection JSUnusedLocalSymbols
   private async created() {
