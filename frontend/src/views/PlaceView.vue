d14cd246d1e7083ff7adf45d9042430187700ebd
Andrei Zhulid <>
2022-11-27 11:48:32 +0100

    Implemented getting place details when page is opened

--- frontend/src/views/PlaceView.vue
+++ frontend/src/views/PlaceView.vue
@@ -1,10 +1,28 @@
 <template>
-  <div>Place Details page</div>
+  <div>
+    <span v-if="place">{{ place.name }}</span>
+    <span v-else>Loading...</span>
+  </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import IPlaceDetailed from '../../../common/interfaces/IPlaceDetailed';
+import SearchService from '@/servises/SearchService';
 
 @Component
-export default class PlaceView extends Vue {}
+export default class PlaceView extends Vue {
+  @Prop({ type: String, required: true }) readonly id!: string;
+
+  private place: IPlaceDetailed | null = null;
+
+  // noinspection JSUnusedLocalSymbols
+  private async created() {
+    try {
+      this.place = await SearchService.searchById(this.id);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
 </script>
